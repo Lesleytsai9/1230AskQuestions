f4c0cd5d45797fbbbf17a67d911e7921edef70f0
Lesleytsai9 <e861k91677@gmail.com>
2019-12-30 15:19:26 +0800

    Initial Commit

---
+++ 1130askQuestion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1130askQuestion
+//
+//  Created by 蔡家雯 on 2019/12/30.
+//  Copyright © 2019 lesley tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
